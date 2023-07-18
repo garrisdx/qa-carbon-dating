5cc1d5d10dc3459ebb7dc2ceb2799d8279eb5847
Dakota <dakotagarrison.14@gmail.com>
2023-07-18 14:21:19 -0500

    first commit

---
+++ src/test/java/Carbon14DatingTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Carbon14DatingTest {
+    //#1 Build a test for calculateAge();
+
+    /*
+        #2 Build a test for setRemainingAmount() in the
+        scenario the given remaining value is <= 1.
+    */
+
+   /*
+        #3 Build a test for setRemainingAmount() in the
+        scenario the given remaining value is >= 1.
+    */
+    @Test
+    @DisplayName("test remaining amount setter with remainder greater than or equal to 1")
+    public void testSetterRemainingAmtRemainGrtOrEqu1(){
+        Carbon14Dating test = new Carbon14Dating(1);
+        assertEquals(1, test.getRemainingAmount());
+    };
+
+
+    // #4 Build a test for the toString() method.
+}
